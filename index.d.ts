1c4a62d5907ec1813990c6240cb35d65f7b8a44f
kotinash <92477814+kotinash@users.noreply.github.com>
2024-02-02 13:43:40 +0100

    Delete index.d.ts

--- index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function parseJson(langString: string): any;
-export declare function parseRaw(content: string): { [key: string]: string };
-export declare function getKey(key: string, jsonObject: any): any;
