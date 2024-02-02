3c9af0e2fdd5c6ccacfe4d8c843ee2a410d2b5c7
kotinash <92477814+kotinash@users.noreply.github.com>
2024-02-02 13:44:06 +0100

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,93 @@
+class LanguageFileParseError extends Error {
+	constructor(message: string) {
+		super()
+
+		this.message = message
+	}
+}
+
+type Language = {
+	[key: string]: string
+}
+
+class LanguageParser {
+	private static REGEX: RegExp = /^\s*([^=]+)\s*=\s*(.*)\s*$/
+
+	static parse_json(lang_str: string) {
+		const lines = lang_str.
+			trim()
+			.split('\n')
+
+		const result = {}
+
+		lines.forEach((line: string) => {
+			const match = line.match(LanguageParser.REGEX)
+
+			if (match) {
+				const key = match[1].trim()
+				const value = match[2].trim()
+
+				const keys = key.split('.')
+				let nested_object = result
+
+				for (let i = 0; i < keys.length; i++) {
+					const current_key = keys[i]
+
+					if (i === keys.length - 1) {
+						nested_object[current_key] = value
+					} else {
+						nested_object[current_key] = nested_object[current_key] || {}
+						nested_object = nested_object[current_key]
+					}
+				}
+			}
+		})
+
+		if (Object.keys(result).length === 0) {
+			throw new LanguageFileParseError("Language file is corrupted or empty")
+		}
+
+		return result
+	}
+
+	static parse_raw(content: string): Language {
+		const translations = {}
+
+		const lines = content.split('\n')
+
+		for (const line of lines) {
+			const trimmedLine = line.trim()
+
+			if (!trimmedLine || trimmedLine.startsWith('#')) {
+				continue // Ignore comments
+			}
+
+			const [key, value] = trimmedLine.split('=')
+			const trimmedKey = key.trim()
+			const trimmedValue = value.trim()
+
+			translations[trimmedKey] = trimmedValue
+		}
+
+		return translations
+	}
+
+	static get_key(key: string, json_object: any) {
+		if (key.includes(".")) {
+			const keys = key.split(".")
+			let value = json_object
+
+			for (const k of keys) {
+				if (!value.hasOwnProperty(k)) {
+					return json_object[key]
+				}
+
+				value = value[k]
+			}
+
+			return value
+		}
+	}
+}
+
+export { LanguageParser, LanguageFileParseError }
